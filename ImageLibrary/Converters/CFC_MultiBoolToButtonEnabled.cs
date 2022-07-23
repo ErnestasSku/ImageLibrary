8cbc0c28c790d354f7db7a9558cf26ecc664da8e
ErnestasSku <ernestassku@gmail.com>
2022-07-23 21:55:00 +0300

    Add try catch to converter

--- ImageLibrary/Converters/CFC_MultiBoolToButtonEnabled.cs
+++ ImageLibrary/Converters/CFC_MultiBoolToButtonEnabled.cs
@@ -13,9 +13,16 @@ internal class CFC_MultiBoolToButtonEnabled : IMultiValueConverter
 {
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        if (values == null || values.Length == 0) return false; 
-        return values.Cast<bool>().All(x => x == true);
+        if (values == null || values.Length == 0) return false;
+        try
+        {
+            return values.Cast<bool>().All(x => x == true);
 
+        }
+        catch (InvalidCastException)
+        {
+            return false;
+        }
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)

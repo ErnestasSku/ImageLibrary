0beebcd219cd1e516e9d521f6bd4b773df9bc5e3
ErnestasSku <ernestassku@gmail.com>
2022-07-24 18:51:29 +0300

    Add experimental namespace

---
+++ ImageLibrary/Experimental/ExperimentWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageLibrary.Experimental
+{
+    /// <summary>
+    /// Interaction logic for ExperimentWindow.xaml
+    /// </summary>
+    public partial class ExperimentWindow : Window
+    {
+        public ExperimentWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ImageLibrary/Experimental/readme.md
@@ -0,0 +1,3 @@
+﻿# Experimental
+
+In this subdirectory new user controls and custom controls will be created and tested. This should be deleted after most of the UI elements are finalized.

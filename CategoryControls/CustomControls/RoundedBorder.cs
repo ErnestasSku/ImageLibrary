6ff79833da8aab2058986b3aebb01f6b94be09dd
ErnestasSku <ernestassku@gmail.com>
2022-07-27 18:49:56 +0300

    Finalize RoundedBorder control

--- CategoryControls/CustomControls/RoundedBorder.cs
+++ CategoryControls/CustomControls/RoundedBorder.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
 using System.Windows.Media;
@@ -7,29 +9,35 @@ namespace CategoryControls.CustomControls;
 
 
 [ContentProperty("Child")]
-public class RoundedBorder : Control
+public class RoundedBorder : Control, INotifyPropertyChanged
 {
-    public static DependencyProperty BorderOffsetProperty =
-        DependencyProperty.Register(
-            nameof(BorderOffset), 
-            typeof(double), 
-            typeof(RoundedBorder), 
-            new PropertyMetadata(0.0));
+    public event PropertyChangedEventHandler? PropertyChanged;
 
+    /// <summary>
+    /// Determines whether the child of this control 
+    /// </summary>
     public static DependencyProperty UseStrictModeProperty =
         DependencyProperty.Register(
-            nameof(UseStrictMode), 
-            typeof(bool), typeof(RoundedBorder), 
-            new PropertyMetadata(false));
-
+            nameof(UseStrictMode),
+            typeof(bool), typeof(RoundedBorder),
+            new PropertyMetadata(
+                false,
+                UseStrictModePropertyChanged));
+    
+    /// <summary>
+    /// Corner radius of the border (and content inside of it using Strict mode)
+    /// </summary>
     public static DependencyProperty CornerRadiusProperty =
         DependencyProperty.Register(
             nameof(CornerRadius), 
             typeof(CornerRadius), 
             typeof(RoundedBorder), 
-            new PropertyMetadata(new CornerRadius(0)));
+            new PropertyMetadata(
+                new CornerRadius(0),
+                CornerRadiusPropertyChanged));
 
-    public static DependencyProperty BackgroundProperty =
+
+    public static new DependencyProperty BackgroundProperty =
         DependencyProperty.Register(
             nameof(Background), 
             typeof(Brush), 
@@ -43,26 +51,24 @@ public class RoundedBorder : Control
             typeof(RoundedBorder), 
             new PropertyMetadata(new SolidColorBrush(Colors.Transparent)));
 
-    public static DependencyProperty BorderThicknesProperty =
+    public static new DependencyProperty BorderThicknessProperty =
         DependencyProperty.Register(
             nameof(BorderThickness), 
             typeof(Thickness), 
             typeof(RoundedBorder), 
-            new PropertyMetadata(new Thickness(0)));
+            new PropertyMetadata(
+                new Thickness(0),
+                BorderThicknesPropertyChanged));
 
     public static DependencyProperty ChildProperty =
         DependencyProperty.Register(
             nameof(Child), 
             typeof(UIElement), 
             typeof(RoundedBorder), 
-            new PropertyMetadata(null));
+            new PropertyMetadata(
+                null,
+                ChildPropertyChanged));
 
-
-    public double BorderOffset
-    {
-        get { return (double)GetValue(BorderOffsetProperty); }
-        set { SetValue(BorderOffsetProperty, value); }
-    }
 
     public bool UseStrictMode
     {
@@ -76,22 +82,22 @@ public class RoundedBorder : Control
         set { SetValue(CornerRadiusProperty, value); }
     }
 
-    public Brush Background
+    public new Brush Background
     {
         get { return (Brush)GetValue(BackgroundProperty); }
         set { SetValue(BackgroundProperty, value); }
     }
 
-    public Brush BorderBrush
+    public new Brush BorderBrush
     {
         get { return (Brush)GetValue(BorderBrushProperty); }
         set { SetValue(BorderBrushProperty, value); }
     }
 
-    public Thickness BorderThickness
+    public new Thickness BorderThickness
     {
-        get { return (Thickness)GetValue(BorderThicknesProperty); }
-        set { SetValue(BorderThicknesProperty, value); }
+        get { return (Thickness)GetValue(BorderThicknessProperty); }
+        set { SetValue(BorderThicknessProperty, value); }
     }
 
     public UIElement Child
@@ -100,11 +106,49 @@ public class RoundedBorder : Control
         set { SetValue(ChildProperty, value); }
     }
 
-    public double calculatedContentWidth { get; set; }
+    private double _calculatedContentWidth;
+    public double CalculatedContentWidth 
+    {
+        get { return _calculatedContentWidth; }
+        set 
+        { 
+            if (_calculatedContentWidth != value)
+            {
+                _calculatedContentWidth = value;
+                NotifyPropertyChanged(nameof(CalculatedContentWidth));
+                
+            }
+        }
+    }
 
-    public double calculatedContentHeight { get; set; }
+    private double _calculatedContentHeight;
+    public double CalculatedContentHeight 
+    {
+        get { return _calculatedContentHeight; }
+        set
+        {
+            if (value != _calculatedContentHeight)
+            {
+                _calculatedContentHeight = value;
+                NotifyPropertyChanged(nameof(CalculatedContentHeight));
 
-    public Geometry contentClip { get; set; }
+            }
+        }
+    }
+
+    private Geometry _contentClip;
+    public Geometry ContentClip 
+    { 
+        get { return _contentClip; }
+        set
+        {
+            if (value != _contentClip)
+            {
+                _contentClip = value;
+                NotifyPropertyChanged(nameof(ContentClip));
+            }
+        }
+    }
 
     static RoundedBorder()
     {
@@ -121,10 +165,7 @@ public class RoundedBorder : Control
     public override void OnApplyTemplate()
     {
         base.OnApplyTemplate();
-        if (UseStrictMode)
-        {
-            CalculateContentValues();
-        }
+        CalculateContentValues();
         if (Child != null)
         {
             Child.ClipToBounds = true;
@@ -133,31 +174,87 @@ public class RoundedBorder : Control
         SizeChanged += RoundedBorder_OnSizeChanged;
     }
 
-    private void RoundedBorder_OnSizeChanged(object sender, SizeChangedEventArgs e)
+
+    private static void UseStrictModePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (UseStrictMode)
+        if (d is RoundedBorder roundedBorder)
         {
-            CalculateContentValues();
+            roundedBorder.UseStrictMode = (bool)e.NewValue;
+            roundedBorder.CalculateContentValues();
         }
     }
 
-    private void CalculateContentValues()
+    private static void CornerRadiusPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-
-        calculatedContentHeight = Height - (BorderThickness.Top + BorderThickness.Bottom);
-        calculatedContentWidth = Width - (BorderThickness.Left + BorderThickness.Right);
-        var clip = new RectangleGeometry(
-            new Rect(0, 0, calculatedContentWidth, calculatedContentHeight), 
-            CornerRadius.TopLeft,
-            CornerRadius.BottomRight);
-        clip.Freeze();
-        contentClip = clip;
-        if (Child != null && Child is FrameworkElement fe)
+        if(d is RoundedBorder roundedBorder)
         {
-            fe.Margin = new Thickness(
-                BorderThickness.Left - BorderThickness.Right, 
-                BorderThickness.Top - BorderThickness.Bottom, 
-                0, 0);
+            roundedBorder.CornerRadius = (CornerRadius)e.NewValue;
+            roundedBorder.CalculateContentValues();
+        }
+    }
+
+    private static void BorderThicknesPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if(d is RoundedBorder roundedBorder)
+        {
+            roundedBorder.BorderThickness = (Thickness)e.NewValue;
+            roundedBorder.CalculateContentValues();
+        }
+    }
+
+    private static void ChildPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is RoundedBorder roundedBorder)
+        {
+            roundedBorder.Child = (UIElement)e.NewValue;
+            roundedBorder.Child.ClipToBounds = roundedBorder.UseStrictMode;
+            roundedBorder.CalculateContentValues();
+        }
+    }
+
+    private void RoundedBorder_OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        CalculateContentValues();
+    }
+
+    public void NotifyPropertyChanged(string propName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+    public void CalculateContentValues()
+    {
+        if (!UseStrictMode)
+        {
+            CalculatedContentHeight = Height;
+            CalculatedContentWidth = Width;
+            ContentClip = Geometry.Empty;
+        }
+        else
+        {
+            CalculatedContentHeight = Height - (BorderThickness.Top + BorderThickness.Bottom);
+            CalculatedContentWidth = Width - (BorderThickness.Left + BorderThickness.Right);
+
+            Geometry clip;
+            try
+            {
+                clip = new RectangleGeometry(
+                new Rect(0, 0, CalculatedContentWidth, CalculatedContentHeight),
+                CornerRadius.TopLeft,
+                CornerRadius.BottomRight);
+            } catch (ArgumentException)
+            {
+                clip = RectangleGeometry.Empty;
+            }
+            
+            clip.Freeze();
+            
+
+            ContentClip = clip;
+            if (Child != null && Child is FrameworkElement fe)
+            {
+                fe.Margin = new Thickness(
+                    BorderThickness.Left - BorderThickness.Right,
+                    BorderThickness.Top - BorderThickness.Bottom,
+                    0, 0);
+            }
         }
     }
 }

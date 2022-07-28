1a699114830969435f6e21831f4883d0889a96e1
ErnestasSku <ernestassku@gmail.com>
2022-07-28 10:22:39 +0300

    Add color utilities

---
+++ ImageLibrary/Utilities/ColorUtilities.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ImageLibrary.Utilities;
+
+public struct HSV
+{
+    public double Hue;
+    public double Saturation;
+    public double Value;
+}
+
+public struct RGB
+{
+    public int Red;
+    public int Green;
+    public int Blue;
+}
+
+public static class ColorUtilities
+{
+    public static HSV RGBtoHSV(int red, int green, int blue)
+    {
+        double redT = red / 255.0;
+        double greenT = green / 255.0;
+        double blueT = blue / 255.0;
+
+        double colorMax = new[] { redT, greenT, blueT }.Max();
+        double colorMin = new[] { redT, greenT, blueT }.Min();
+        double delta = colorMax - colorMin;
+
+        double hue, saturation, value;
+
+        if (colorMax == colorMin)
+        {
+            hue = 0;
+        } 
+        else if (colorMax == redT)
+        {
+            hue = (60 * ((greenT - blueT) / delta) + 360) % 360;
+        }
+        else if (colorMax == greenT)
+        {
+            hue = (60 * ((blueT - redT) / delta) + 120) % 360;
+        }
+        else if (colorMax == blueT)
+        {
+            hue = (60 * ((redT - greenT) / delta) + 240) % 360;
+        } 
+        else
+        {
+            hue = 0;
+        }
+        
+        if (colorMax == 0)
+        {
+            saturation = 0;
+        }
+        else
+        {
+            saturation = (delta / colorMax) * 100;
+        }
+
+        value = colorMax * 100;
+        return new HSV() { Hue = hue, Saturation = saturation, Value = value };
+
+    }
+
+    public static HSV RGBtoHSV(RGB values)
+    {
+        return RGBtoHSV(values.Red, values.Green, values.Blue);
+    }
+
+    public static RGB HSVtoRGB(double hue, double saturation, double value)
+    {
+
+        double color = value * saturation;
+        double X = color * (1 - Math.Abs(hue / 60) % 2 - 1) % 360;
+        double m = value - color;
+
+        var (rT, gT, bT) = calculateRGBPrime(color, X, hue);
+
+        RGB rgb = new RGB()
+        {
+            Red = (int)(rT + m) * 255,
+            Green = (int)(gT + m) * 255,
+            Blue = (int)(bT + m) * 255
+        };
+        return rgb;
+    }
+
+    public static RGB HSVtoRGB(HSV values)
+    {
+        return HSVtoRGB(values.Hue, values.Saturation, values.Value);
+    }
+
+    private static (double, double, double) calculateRGBPrime(double color, double x, double hue)
+    {
+        if (hue <= 0 && hue < 60)
+        {
+            return (color, x, 0);
+        }
+        else if (hue <= 60 && hue < 120)
+        {
+            return (x, color, 0);
+        }
+        else if (hue <= 120 && hue < 180)
+        {
+            return (0, color, x);
+        }
+        else if (hue <= 180 && hue < 240)
+        {
+            return (0, x, color);
+        }
+        else if (hue <= 240 && hue < 300)
+        {
+            return (x, 0, color);
+        }
+        else if (hue <= 300 && hue < 360)
+        {
+            return (color, 0, x);
+        }
+        else
+        {
+            throw new ArgumentException(message:"Hue should always be in the range specified above");
+        }
+    }
+
+    public static RGB HEXtoRGB(string hex)
+    {
+        return new RGB()
+        {
+            Red = Convert.ToInt32(Regex.Matches(hex, @"[0-9a-fA-F]{2}")[0].Value, 16),
+            Green = Convert.ToInt32(Regex.Matches(hex, @"[0-9a-fA-F]{2}")[1].Value, 16),
+            Blue = Convert.ToInt32(Regex.Matches(hex, @"[0-9a-fA-F]{2}")[2].Value, 16)
+        };
+    }
+
+    public static string RGBtoHEX(RGB value)
+    {
+        return RGBtoHEX(value.Red, value.Green, value.Blue);
+    }
+
+    public static string RGBtoHEX(int red, int green, int blue)
+    {
+        return Convert.ToString(red, 16).PadLeft(2, '0') +
+               Convert.ToString(green, 16).PadLeft(2, '0') +
+               Convert.ToString(blue, 16).PadLeft(2, '0');
+    }
+}

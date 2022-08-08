92428f207617571da1f008a027fdd0159944fefc
ErnestasSku <ernestassku@gmail.com>
2022-08-08 17:41:30 +0300

    Update db Tag type table

--- ImageLibrary/Database/Models/TagMetaType.cs
+++ ImageLibrary/Database/Models/TagMetaType.cs
@@ -8,41 +8,10 @@ public class TagMetaType
 {
     [Key]
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Background { get; set; } = "FFFFFF";
+    public string Foreground { get; set; } = "000000";
 
     public List<Tag> Tags { get; set; }
-
-    private int _red;
-    public int Red
-    {
-        get { return _red; }
-        set
-        {
-            if (value >= 0 && value <= 255)
-                _red = value;
-        }
-    }
-
-    private int _blue;
-    public int Blue
-    {
-        get { return _blue; }
-        set
-        {
-            if (value > 0 && value <= 255)
-                _blue = value;
-        }
-    }
-
-    private int _green;
-    public int Green
-    {
-        get { return _green; }
-        set
-        {
-            if (value < 0 && value <= 255)
-                _green = value;
-        }
-    }
 }

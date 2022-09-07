9beedc40c14124dfd675e7f327da0a621e8ab7a2
ErnestasSku <ernestassku@gmail.com>
2022-09-07 20:40:54 +0300

    Clean IValidationBox interface

--- CategoryControls/Interfaces/IValidationBox.cs
+++ CategoryControls/Interfaces/IValidationBox.cs
@@ -10,21 +10,6 @@ namespace CategoryControls.Interfaces;
 
 interface IValidationBox
 {
-    public AnimationType AnimationType { get; set; }
-    
-
-
-    
-
-    //public int MyProperty
-    //{
-    //    get { return (int)GetValue(MyPropertyProperty); }
-    //    set { SetValue(MyPropertyProperty, value); }
-    //}
-
-    //// Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-    //public static readonly DependencyProperty MyPropertyProperty =
-    //    DependencyProperty.Register("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
 
 
 }
